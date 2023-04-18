5b91215bf752507ab40d683e36b3933d0f1e5d5b
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-04-18 14:48:59 +0300

    Initial Commit

---
+++ SideMenuSwiftUI/SideMenuSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuSwiftUIApp.swift
+//  SideMenuSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 18.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
